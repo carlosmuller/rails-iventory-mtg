e90e1e796698d24e1bbb16851d83f243fd79ed2b
vm <kdu428@gmail.com>
2017-04-21 01:50:31 +0000

    Separate logic from controller
    TODO: Tests and seed on seeds.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   require 'net/http'
   require 'json'
+  include ApplicationHelper
 
   def add_set
     current_set = getSet params[:set]
@@ -24,56 +25,11 @@ class ApplicationController < ActionController::Base
     allsets = getAllSets
     Concurrent::Future.execute do
       allsets.each do |set|
-        processSet(set)
+       processSet(set)
       end
       logger.info "Terminei o update"
     end
     head 200
   end
 
-  def processSet(set)
-    setName = set['name']
-    logger.info "Começando a processar o set[#{setName}]"
-    sets = Sets.find_by(name: setName)
-    if !sets
-      sets = Sets.new
-      sets.name= setName
-      sets.save
-    end
-    set['cards'].each do |card|
-      processCard(card, sets)
-    end
-    logger.info "Terminei de processar o set[#{setName}]"
-  end
-
-  def processCard(card, sets)
-    cardName = card['name']
-    logger.info "Começando a processar o card[#{cardName}]"
-    card_find_by = Card.find_by(name: cardName)
-    if card_find_by
-      card_find_by.merge card
-      card_find_by.save
-      logger.info "Fiz update no card[#{cardName}]"
-    else
-      card_find_by = Card.new
-      card_find_by.createFromJson(card)
-      card_find_by.save
-      logger.info "Criei o card[#{cardName}]"
-    end
-    card_set = CardSet.new
-    card_set.card = card_find_by
-    card_set.sets = sets
-    card_set.mid = card['multiverseid']
-    card_set.save
-  end
-
-  def getSet set
-    response = Net::HTTP.get_response('mtgjson.com', "/json/#{set}.json")
-    return JSON.parse(response.body)
-  end
-
-  def getAllSets
-    response = Net::HTTP.get_response('mtgjson.com', '/json/AllSetsArray-x.json')
-    return JSON.parse(response.body)
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+
+ def processSet(set)
+    setName = set['name']
+    logger.info "Começando a processar o set[#{setName}]"
+    sets = Sets.find_by(name: setName)
+    if !sets
+      sets = Sets.new
+      sets.name= setName
+      sets.save
+    end
+    set['cards'].each do |card|
+      processCard(card, sets)
+    end
+    logger.info "Terminei de processar o set[#{setName}]"
+  end
+
+  def processCard(card, sets)
+    cardName = card['name']
+    logger.info "Começando a processar o card[#{cardName}]"
+    card_find_by = Card.find_by(name: cardName)
+    if card_find_by
+      card_find_by.merge card
+      card_find_by.save
+      logger.info "Fiz update no card[#{cardName}]"
+    else
+      card_find_by = Card.new
+      card_find_by.createFromJson(card)
+      card_find_by.save
+      logger.info "Criei o card[#{cardName}]"
+    end
+    card_set = CardSet.new
+    card_set.card = card_find_by
+    card_set.sets = sets
+    card_set.mid = card['multiverseid']
+    card_set.save
+  end
+  
+  def getSet (set)
+    response = Net::HTTP.get_response('mtgjson.com', "/json/#{set}.json")
+    return JSON.parse(response.body)
+  end
+
+  def getAllSets()
+    response = Net::HTTP.get_response('mtgjson.com', '/json/AllSetsArray-x.json')
+    return JSON.parse(response.body)
+  end
+
 end
