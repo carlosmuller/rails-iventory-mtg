86c43cf64ddbdc5125f73ce16bfacd36914e8edc
carlosmuller <kdu428@gmail.com>
2017-04-21 16:47:01 +0000

    Removing unecessary log

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,9 +2,6 @@ class CardsController < ApplicationController
 
   def search
     cards = Card.where('name ILIKE ?', "%#{params[:query]}%")
-    cards.each do |card|
-      logger.info card
-    end
     render json: cards, except: [:id, :created_at, :update_at, :mid, :sets], include: [:card_sets]
   end
 end
