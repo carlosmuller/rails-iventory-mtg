ba41602d12be4de736618267c6dd59a59e4c8254
carlosmuller <kdu428@gmail.com>
2017-04-24 20:34:17 -0300

    revert network

--- config/boot.rb
+++ config/boot.rb
@@ -4,8 +4,8 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 require 'rails/commands/server'
 module Rails
-  class Server
-    def default_options
-      super.merge(Host: '0.0.0.0', Port: 3000)
-    end
-  end
+  # class Server
+  #   def default_options
+  #     super.merge(Host: '0.0.0.0', Port: 3000)
+  #   end
+  # end
