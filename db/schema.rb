775641469aadc80dfbe0f41640ec3bfcf56abccc
carlosmuller <kdu428@gmail.com>
2017-04-30 09:59:20 -0300

    Initial user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424221445) do
+ActiveRecord::Schema.define(version: 20170430124956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20170424221445) do
     t.datetime "updated_at", null: false
     t.string   "code"
     t.index ["code"], name: "index_sets_on_code", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
